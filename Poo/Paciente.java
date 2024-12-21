88a45fe949617839bec1afa3d4bb575ff70ced1e
Gabriella Marinho <gabriellamarilia30@gmail.com>
2024-12-21 19:41:15 -0300

    upload changes

--- Poo/Paciente.java
+++ Poo/Paciente.java
@@ -1,15 +1,16 @@
 package Poo;
 
 public class Paciente {
+    private int codigo;
     private String nomePaciente;
     private int idadePaciente;
     private String dataNascimento;
-    private char sexo;
+    private String sexo;
     private String planoSaude;
     private String alergia;
     private String tipoSanguineo;
 
-Public paciente (String nome , int idadePaciente, String dataNascimento, String sexo , String planoSaude, String alergia,String tipoSanguineo ){
+Public paciente (int codigo, String nome , int idadePaciente, String dataNascimento, String sexo , String planoSaude, String alergia,String tipoSanguineo ){
     this.nomePaciente=nomePaciente;
     this.idadePaciente=idadePaciente;
     this.dataNascimento=dataNascimento;
@@ -17,10 +18,17 @@ Public paciente (String nome , int idadePaciente, String dataNascimento, String 
     this.planoSaude=planoSaude;
     this.alergia=alergia;
     this.tipoSanguineo=tipoSanguineo;
-}
+};
 
 
+    public int getCodigo(){
+        return codigo;
 
+    };
+
+    public void setCodigo(setCodigo){
+        
+    }
     ATRIBUTOS: 
 // codigo, nome, dataNascimento, sexo, planoSaude, alergia, tipoSanguineo.
 
