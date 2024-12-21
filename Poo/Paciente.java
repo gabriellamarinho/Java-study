91ff1fc6fa066054661ac60d94c65930ea11271f
Gabriella Marinho <gabriellamarilia30@gmail.com>
2024-12-21 20:49:09 -0300

    upload changes

--- Poo/Paciente.java
+++ Poo/Paciente.java
@@ -10,26 +10,78 @@ public class Paciente {
     private String alergia;
     private String tipoSanguineo;
 
-Public paciente (int codigo, String nome , int idadePaciente, String dataNascimento, String sexo , String planoSaude, String alergia,String tipoSanguineo ){
-    this.nomePaciente=nomePaciente;
-    this.idadePaciente=idadePaciente;
-    this.dataNascimento=dataNascimento;
-    this.sexo=sexo;
-    this.planoSaude=planoSaude;
-    this.alergia=alergia;
-    this.tipoSanguineo=tipoSanguineo;
-};
-
-
-    public int getCodigo(){
-        return codigo;
-
-    };
-
-    public void setCodigo(setCodigo){
-        
+    public Paciente(int codigo, String nomePaciente, int idadePaciente, String dataNascimento, String sexo, String planoSaude, String alergia, String tipoSanguineo) {
+        this.codigo = codigo;
+        this.nomePaciente = nomePaciente;
+        this.idadePaciente = idadePaciente;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.planoSaude = planoSaude;
+        this.alergia = alergia;
+        this.tipoSanguineo = tipoSanguineo;
     }
-    ATRIBUTOS: 
-// codigo, nome, dataNascimento, sexo, planoSaude, alergia, tipoSanguineo.
 
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNomePaciente() {
+        return nomePaciente;
+    }
+
+    public void setNomePaciente(String nomePaciente) {
+        this.nomePaciente = nomePaciente;
+    }
+
+    public int getIdadePaciente() {
+        return idadePaciente;
+    }
+
+    public void setIdadePaciente(int idadePaciente) {
+        this.idadePaciente = idadePaciente;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getPlanoSaude() {
+        return planoSaude;
+    }
+
+    public void setPlanoSaude(String planoSaude) {
+        this.planoSaude = planoSaude;
+    }
+
+    public String getAlergia() {
+        return alergia;
+    }
+
+    public void setAlergia(String alergia) {
+        this.alergia = alergia;
+    }
+
+    public String getTipoSanguineo() {
+        return tipoSanguineo;
+    }
+
+    public void setTipoSanguineo(String tipoSanguineo) {
+        this.tipoSanguineo = tipoSanguineo;
+    }
 }
