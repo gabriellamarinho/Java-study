88ac29de760d45ee155c989ec9ef766ac6230eea
Gabriella Marinho <gabriellamarilia30@gmail.com>
2024-12-21 19:24:53 -0300

    upload changes

--- Desafios de logica/Fibonacci.java
+++ Desafios de logica/Fibonacci.java
@@ -1,8 +1,8 @@
-public class Fibonacci {
-    public static void main(String[] args) {
-        System.out.print("Olá vamos iniciar nossa sequencia de fibonacci , ate qual ");
+// public class Fibonacci {
+//     public static void main(String[] args) {
+//         System.out.print("Olá vamos iniciar nossa sequencia de fibonacci , ate qual ");
 
-        int iniciosequencia=1;
-        int numerosequencia=1;
-    }
-}
+//         int iniciosequencia=1;
+//         int numerosequencia=1;
+//     }
+// }
